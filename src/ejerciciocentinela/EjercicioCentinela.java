faf4f0dc9fd8467d371f16769e8c38fb31980042
alainmelendez <alainjeshua@gmail.com>
2023-02-10 07:42:44 -0300

    Proyecto Ejercicio Centinela terminado

---
+++ src/ejerciciocentinela/EjercicioCentinela.java
@@ -0,0 +1,27 @@
+
+package ejerciciocentinela;
+
+import java.util.Scanner;
+
+
+public class EjercicioCentinela {
+
+   
+    public static void main(String[] args) {
+       //sabemos que es un bucle
+       //sabemos la condicion de salida
+       //que estructura usar
+       
+        System.out.println("Ingrese una palabra");
+        Scanner teclado = new Scanner (System.in);
+        String palabra = teclado.next();
+        
+        while (!palabra.equalsIgnoreCase("salir")) {
+            System.out.println("La palabra es: " + palabra);
+            
+            System.out.println("Ingrese una palabra");
+            palabra = teclado.next();
+        }
+    }
+    
+}
